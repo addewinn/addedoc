a647db8baa40140c3187ddb94d155da410997d52
Kevin Winn <kevinwinn@advisorpeak.com>
2022-05-15 18:55:13 -0400

    Update default home and author pages

--- src/org/addepar/addedoc/AddeDoc.java
+++ src/org/addepar/addedoc/AddeDoc.java
@@ -53,8 +53,8 @@ public class AddeDoc {
     public static void RunAddeDoc(String[] args, IProgressMonitor monitor) {
         String sourceDirectory = "";
         String targetDirectory = "";
-        String homefilepath = "home.html";
-        String authorfilepath = "author.html";
+        String homefilepath = "";
+        String authorfilepath = "";
         String hostedSourceURL = "";
 
         // parse command line parameters
